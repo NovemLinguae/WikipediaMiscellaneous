db9faee57685f0d6fb0bfc675ea3bc2f423014cb
NovemLinguae <novemlinguae@gmail.com>
2024-05-28 05:40:18 -0700

    add "skip downloading libraries" mode

--- bash/install-extension.sh
+++ bash/install-extension.sh
@@ -8,16 +8,13 @@ docker compose up -d
 echo "What's the name of the extension? Capitalize it correctly please."
 read -r extensionName
 
+# git clone only?
+echo "Do you want to skip downloading libraries, updating SQL database, etc. and just git clone? y/n"
+read -r gitCloneOnly
+
 # git clone
 cd ~/mediawiki/extensions || exit
 git clone "ssh://novemlinguae@gerrit.wikimedia.org:29418/mediawiki/extensions/$extensionName"
-
-# composer update
-docker compose exec mediawiki composer update --working-dir "extensions/$extensionName"
-
-# npm ci
-cd "$HOME/mediawiki/extensions/$extensionName" || exit
-npm ci
 
 # make .vscode/settings.json file. so that when extension is open in IDE, intellisense loads type hints for objects from mediawiki core
 cd "$HOME/mediawiki/extensions/$extensionName" || exit
@@ -26,20 +23,30 @@ cd "$HOME/mediawiki/extensions/$extensionName/.vscode" || exit
 touch settings.json
 printf "{\n\t\"intelephense.environment.includePaths\": [\n\t\t\"../../\"\n\t]\n}\n" >> settings.json
 
-# add wfLoadExtension to LocalSettings.php
-cd ~/mediawiki || exit
-echo "wfLoadExtension( '$extensionName' );" >> LocalSettings.php
+if [ "$gitCloneOnly" != "y" ]; then
+	# composer update
+	docker compose exec mediawiki composer update --working-dir "extensions/$extensionName"
 
-# composer update for mediawiki core, so that the next step doesn't freak out
-cd ~/mediawiki || exit
-docker compose exec mediawiki composer update
+	# npm ci
+	cd "$HOME/mediawiki/extensions/$extensionName" || exit
+	npm ci
 
-# update the SQL database
-cd ~/mediawiki || exit
-docker compose exec mediawiki php maintenance/run.php update
+	# add wfLoadExtension to LocalSettings.php
+	cd ~/mediawiki || exit
+	echo "wfLoadExtension( '$extensionName' );" >> LocalSettings.php
 
-# cd to the extension's directory. for easy git reviewing
-cd "$HOME/mediawiki/extensions/$extensionName" || exit
+	# composer update for mediawiki core, so that the next step doesn't freak out
+	cd ~/mediawiki || exit
+	docker compose exec mediawiki composer update
+
+	# update the SQL database
+	cd ~/mediawiki || exit
+	docker compose exec mediawiki php maintenance/run.php update
+fi
 
 # open VS Code for this extension
+cd "$HOME/mediawiki/extensions/$extensionName" || exit
 code .
+
+# cd to the extension's directory. for easy `git review`ing
+cd "$HOME/mediawiki/extensions/$extensionName" || exit
